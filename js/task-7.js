f049c57375d2e831f67b593e4b06f8fb0096e133
Roman Fedevych <fedevychr@gmail.com>
2023-12-02 15:12:22 +0200

    task-7

---
+++ js/task-7.js
@@ -0,0 +1,23 @@
+// У нас є об'єкт, у якому зберігаються зарплати
+//    нашої команди
+//    Напишіть код для підсумовування всіх зарплат і
+//    збережіть його результат у змінній sum.
+//    Якщо об'єкт salaries порожній, то результат має бути 0
+
+const salaries = {
+  Mango: 100,
+  Poly: 160,
+  Ajax: 1470,
+};
+
+function sumSalaries(salaries) {
+    let sum = 0;
+
+    for (const value of Object.values(salaries)) {
+        sum += value;
+    }
+    return sum;
+}
+
+
+console.log(sumSalaries(salaries));
